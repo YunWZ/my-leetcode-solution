502054e6925badf21970337644a37db4323a0f34
YunWZ <verdgun@gmail.com>
2021-10-09 22:21:10 +0800

    修改结构体ListNode#toString()方法；

--- src/main/java/cn/com/yunweizhan/common/entity/ListNode.java
+++ src/main/java/cn/com/yunweizhan/common/entity/ListNode.java
@@ -1,39 +1,51 @@
 package cn.com.yunweizhan.common.entity;
 
 public class ListNode {
-    public int val;
-    public ListNode next;
+	public int val;
+	public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
-    }
+	public ListNode(int x) {
+		val = x;
+	}
 
-    public ListNode(int x, ListNode next) {
-        val = x;
-        this.next = next;
-    }
+	public ListNode(int x, ListNode next) {
+		val = x;
+		this.next = next;
+	}
 
-    public ListNode next(ListNode next) {
-        this.next = next;
-        return this;
-    }
+	public static ListNode builder(int[] nodeVals) {
+		ListNode head = new ListNode(nodeVals[0]), tail = head;
+		for (int i = 1; i < nodeVals.length; i++) {
+			tail.next = new ListNode(nodeVals[i]);
+			tail = tail.next;
+		}
 
-    public static ListNode builder(int i, ListNode next) {
-        ListNode listNode = new ListNode(i);
-        listNode.next = next;
-        return listNode;
-    }
+		return head;
+	}
 
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(val);
-        ListNode temp = next;
-        while (temp != null) {
-            stringBuilder.append("->").append(temp.val);
-            temp = temp.next;
-        }
+	public ListNode next(ListNode next) {
+		this.next = next;
+		return this;
+	}
 
-        return stringBuilder.toString();
-    }
+	public static ListNode builder(int i, ListNode next) {
+		ListNode listNode = new ListNode(i);
+		listNode.next = next;
+		return listNode;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(val).append("->");
+
+		if (next != null) {
+			stringBuilder.append(next);
+		}
+		else {
+			stringBuilder.append("null");
+		}
+
+		return stringBuilder.toString();
+	}
 }
