e3ef491fc96511dafde44e61c1e8207c858da50e
WeizhanYun <yunweizhan@gmail.com>
2022-03-14 18:37:24 +0800

    删除无用代码

--- src/main/java/jvmspec/inte/InterfaceA.java
+++
@@ -1,9 +0,0 @@
-package jvmspec.inte;
-
-public interface InterfaceA {
-    int a = 2;
-
-    static  int getB() {
-        return 11;
-    }
-}
--- src/main/java/jvmspec/klass/Father.java
+++
@@ -1,8 +0,0 @@
-package jvmspec.klass;
-
-public class Father {
-        public int a = 1;
-    public int getB() {
-        return 1;
-    }
-}
