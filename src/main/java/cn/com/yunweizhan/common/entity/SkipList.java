829e75c01b4829fe1af57a600bd652f2b1b4f83c
WeizhanYun <yunweizhan@gmail.com>
2022-04-06 20:28:52 +0800

    增加数据结构--滑动窗口/跳表

---
+++ src/main/java/cn/com/yunweizhan/common/entity/SkipList.java
@@ -0,0 +1,162 @@
+package cn.com.yunweizhan.common.entity;
+
+import java.util.Comparator;
+import java.util.Random;
+
+/**
+ * @author Weizhan▪Yun
+ * @date 2022/4/5 18:25
+ */
+public class SkipList<T extends Comparable<T>> {
+	public static final int MAX_LEVEL = 32;
+	private int level;
+	private int length;
+	private SkipListNode<T> header, tail;
+	private final Random random = new Random(System.currentTimeMillis());
+	private Comparator<T> comparator;
+
+	public SkipList() {
+		this(null);
+	}
+
+	public SkipList(Comparator<T> comparator) {
+		this.level = randomLevel();
+		this.header = new SkipListNode<>(null, null, this.level);
+		this.tail = null;
+		this.length = 0;
+		this.comparator = comparator;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void add(T value) {
+		if (null == value) {
+			return;
+		}
+		int i, level;
+		SkipListNode<T> x = this.header;
+		SkipListNode<T>[] update = new SkipListNode[MAX_LEVEL];
+
+		for (i = this.level - 1; i >= 0; i--) {
+			while (x.level[i].forward != null
+					&& compare(x.level[i].forward.value, value) < 0) {
+				x = x.level[i].forward;
+			}
+			update[i] = x;
+		}
+
+		level = randomLevel();
+		for (int j = this.level; j < level; j++) {
+			update[j] = this.header;
+		}
+		x = new SkipListNode<>(value, null, level);
+		this.level = level;
+
+		for (int j = 0; j < level; j++) {
+			x.level[j].forward = update[j].level[j].forward;
+			update[j].level[j].forward = x;
+		}
+
+		x.backward = update[0] == this.header ? null : update[0];
+
+		if (x.level[0].forward != null) {
+			x.level[0].forward.backward = x;
+		}
+		else {
+			this.tail = x;
+		}
+
+		this.length++;
+	}
+
+	public boolean has(T value) {
+		SkipListNode<T> x = this.header;
+
+		int level = this.level;
+
+		for (int i = level - 1; i >= 0; i--) {
+			while (x.level[i].forward != null && compare(x.level[i].forward.value, value) < 0) {
+				x = x.level[i].forward;
+			}
+		}
+
+		x = x.level[0].forward;
+		return x != null && compare(x.value, value) == 0;
+	}
+
+	public void delete(T value) {
+		SkipListNode<T> x = this.header;
+		SkipListNode<T>[] update = new SkipListNode[MAX_LEVEL];
+		for (int i = this.level - 1; i >= 0; i--) {
+			while (x.level[i].forward != null && compare(x.level[i].forward.value, value) < 0) {
+				x = x.level[i].forward;
+			}
+			update[i] = x;
+		}
+
+		x = x.level[0].forward;
+		if (compare(x.value, value) != 0) {
+			return;
+		}
+
+		for (int i = 0; i < this.level; i++) {
+			x.level[i].forward.backward = update[i];
+			update[i].level[i].forward = x.level[i].forward;
+		}
+
+		while (this.level > 1 && this.header.level[this.level].forward == null) {
+			this.level--;
+		}
+		this.length--;
+	}
+
+	private int compare(T v1, T v2) {
+		return comparator != null ? comparator.compare(v1, v2) : v1.compareTo(v2);
+	}
+
+	public static <T extends Comparable<T>> SkipList<T> skipList() {
+		return new SkipList<>();
+	}
+
+	public class SkipListNode<T> {
+		private T value;
+		private SkipListNode<T> backward;
+		private SkipListNodeLevel<T>[] level;
+
+		SkipListNode(T value, SkipListNode<T> backward, int level) {
+			this.value = value;
+			this.backward = backward;
+			this.level = new SkipListNodeLevel[level];
+		}
+
+		public T getValue() {
+			return value;
+		}
+
+		public SkipListNode<T> getBackward() {
+			return backward;
+		}
+
+		public SkipListNodeLevel<T>[] getLevel() {
+			return level;
+		}
+	}
+
+	private int randomLevel() {
+		var level = 1;
+		while (random.nextBoolean()) {
+			level++;
+		}
+		return level;
+	}
+
+	public class SkipListNodeLevel<T> {
+		private SkipListNode<T> forward;
+
+		public SkipListNode<T> getForward() {
+			return forward;
+		}
+	}
+}
---
+++ src/main/java/cn/com/yunweizhan/common/entity/Window.java
@@ -0,0 +1,65 @@
+package cn.com.yunweizhan.common.entity;
+
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+/**
+ * @author Weizhan▪Yun
+ * @date 2022/4/5 17:44
+ */
+public class Window {
+	private int sampleCount;
+	private int intervalInMs;
+	private long windowLenghtInMs;
+	private AtomicReferenceArray<WindowWrp<Integer>> array;
+
+	public Window(int sampleCount, int intervalInMs) {
+		this.sampleCount = sampleCount;
+		this.intervalInMs = intervalInMs;
+		this.windowLenghtInMs = intervalInMs / sampleCount;
+		this.array = new AtomicReferenceArray<>(sampleCount);
+	}
+
+	private int getTimeIdx(long timestamp) {
+		var timeIdx = timestamp / windowLenghtInMs;
+		return (int) timeIdx % array.length();
+	}
+
+	public long getWindowStart(long timestamp) {
+		return timestamp - timestamp % windowLenghtInMs;
+	}
+
+	public void put(long timestamp, int count) {
+		if (timestamp < 0) {
+			return;
+		}
+		int timeIdx = getTimeIdx(timestamp);
+		long windowStart = getWindowStart(timestamp);
+		WindowWrp<Integer> integerWindowWrp = array.get(timeIdx);
+
+		while (true) {
+			if (integerWindowWrp.windowStart == windowStart) {
+				integerWindowWrp.value++;
+				return;
+			}
+			else if (integerWindowWrp.windowStart > windowStart) {
+				return;
+			}
+			else {
+				integerWindowWrp.windowStart = windowStart;
+				integerWindowWrp.value = 0;
+			}
+		}
+	}
+
+	public class WindowWrp<T> {
+		private final long windowLenghtInMs;
+		private long windowStart;
+		private T value;
+
+		WindowWrp(long windowLenghtInMs, long windowStart, T value) {
+			this.windowLenghtInMs = windowLenghtInMs;
+			this.windowStart = windowStart;
+			this.value = value;
+		}
+	}
+}
